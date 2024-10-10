1bd7c80c31fda90967d79df63e1a4b71cc9ed0f8
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:26:24 +0600

    Update Milk.java

--- 210042125_DP_Lab_Task4/src/Condiment/Milk.java
+++ 210042125_DP_Lab_Task4/src/Condiment/Milk.java
@@ -1,5 +1,16 @@
 package Condiment;
 
+import Beverage.Beverage;
+
 public class Milk extends Condiment{
 
+    public Milk(Condiment c){
+        super(c);
+    }
+    public Milk(){
+
+    }
+    public Milk(Beverage b){
+        super(b);
+    }
 }
