04fd18a76fb73c9bb1cdb0cc6febbef202cefb4a
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:39:57 +0600

    Update Latte.java

--- 210042125_DP_Lab_Task4/src/Beverage/Latte.java
+++ 210042125_DP_Lab_Task4/src/Beverage/Latte.java
@@ -1,4 +1,17 @@
 package Beverage;
 
+import Condiment.Condiment;
+
 public class Latte extends Beverage{
+    public Latte(Beverage b){
+        super(b);
+    }
+
+    public Latte(Condiment c){
+        super(c);
+    }
+
+    public Latte(){
+
+    }
 }
