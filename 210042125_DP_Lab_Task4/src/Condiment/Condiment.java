53bc1f90f907bb7a24ff852114c822364d4cc23c
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 02:19:19 +0600

    Update Condiment

--- 210042125_DP_Lab_Task4/src/Condiment/Condiment.java
+++ 210042125_DP_Lab_Task4/src/Condiment/Condiment.java
@@ -1,8 +1,19 @@
 package Condiment;
 
-public class Condiment {
-    public void addCondiment(Condiment condiment){
+import Beverage.Beverage;
 
+public class Condiment {
+
+    public Condiment(Condiment c){
+        System.out.println("added "+c.getClass().getSimpleName()+" to "+this.getClass().getSimpleName());
+    }
+
+    public Condiment(){
+
+    }
+
+    public Condiment(Beverage b){
+        System.out.println("added "+b.getClass().getSimpleName()+" to "+this.getClass().getSimpleName());
     }
 
 
