07fc521f9df71f229a27091bc2555c4a10da6905
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:41:37 +0600

    Update Main.java

--- 210042125_DP_Lab_Task4/src/Main.java
+++ 210042125_DP_Lab_Task4/src/Main.java
@@ -1,9 +1,28 @@
 import Beverage.Beverage;
 import Condiment.Milk;
+import Condiment.WhippedCream;
+import Condiment.WhiteSugar;
+import Factory.CondimentFactory;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Beverage b= new Beverage();
-        b.addCondiment("Milk");
+
+        CondimentFactory cf= new CondimentFactory();
+       Scanner j= new Scanner(System.in);
+       int i= j.nextInt();
+
+        switch (i) {
+            case 1:
+               cf.createCondiment("Milk over Sugar");
+               break;
+            case 2:
+                cf.createCondiment("Sugar over Milk");
+                break;
+            case 3:
+                cf.createCondiment("Milk over Cream over Espresso");
+                break;
+        }
     }
 }
