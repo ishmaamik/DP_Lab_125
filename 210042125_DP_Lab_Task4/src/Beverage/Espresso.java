eb5a3261abaeef3d43bb068710018d2a192ec9e7
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:40:30 +0600

    Update Espresso.java

--- 210042125_DP_Lab_Task4/src/Beverage/Espresso.java
+++ 210042125_DP_Lab_Task4/src/Beverage/Espresso.java
@@ -1,4 +1,17 @@
 package Beverage;
 
+import Condiment.Condiment;
+
 public class Espresso extends Beverage{
+        public Espresso(Beverage b){
+            super(b);
+        }
+
+    public Espresso(Condiment c){
+        super(c);
+    }
+
+    public Espresso(){
+
+    }
 }
