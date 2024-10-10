4a6460e48c696a98a7529150e0e6db6e51e6bac2
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:28:04 +0600

    Update WhiteSugar.java

--- 210042125_DP_Lab_Task4/src/Condiment/WhiteSugar.java
+++ 210042125_DP_Lab_Task4/src/Condiment/WhiteSugar.java
@@ -1,4 +1,17 @@
 package Condiment;
 
+import Beverage.Beverage;
+
 public class WhiteSugar extends Condiment{
+    public WhiteSugar(){
+
+    }
+
+    public WhiteSugar(Condiment c){
+        super(c);
+    }
+
+    public WhiteSugar(Beverage b){
+        super(b);
+    }
 }
