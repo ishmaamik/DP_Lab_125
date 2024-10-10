6b64d677c61172cf03a38fa398d7b4f7bdb9f566
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:27:01 +0600

    Update WhippedCream.java

--- 210042125_DP_Lab_Task4/src/Condiment/WhippedCream.java
+++ 210042125_DP_Lab_Task4/src/Condiment/WhippedCream.java
@@ -1,4 +1,18 @@
 package Condiment;
 
+import Beverage.Beverage;
+
 public class WhippedCream extends Condiment{
+
+    public WhippedCream(Condiment c){
+        super(c);
+    }
+
+    public WhippedCream(){
+
+    }
+
+    public WhippedCream(Beverage b){
+        super(b);
+    }
 }
