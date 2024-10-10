b06ec45b023d41253709d9b07acdec3af879073d
ishmaamik <128014789+ishmaamik@users.noreply.github.com>
2024-10-11 03:36:30 +0600

    Update Beverage.java

--- 210042125_DP_Lab_Task4/src/Beverage/Beverage.java
+++ 210042125_DP_Lab_Task4/src/Beverage/Beverage.java
@@ -1,18 +1,8 @@
 package Beverage;
 
 import Condiment.Condiment;
-import Condiment.Milk;
-import Condiment.Milk;
-import Condiment.Milk;
-import Factory.CondimentFactory;
 
 public class Beverage {
-    int total_cost=90;
-    public void addCondiment(String c){
-        CondimentFactory cf= new CondimentFactory();
-        Condiment a= cf.createCondiment(c);
-        System.out.println("Condiment"+c+"added to Beverage and total cost is "+ total_cost);
-    }
 
     public Beverage(Beverage b){
         System.out.println("added "+b.getClass().getSimpleName()+" to "+this.getClass().getSimpleName());
