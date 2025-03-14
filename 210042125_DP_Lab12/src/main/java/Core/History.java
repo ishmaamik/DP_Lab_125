5cc6c96e8fa470b72a1b42f9e7556e112a2d38d0
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-14 14:36:43 +0600

    #4 done

--- 210042125_DP_Lab12/src/main/java/Core/History.java
+++ 210042125_DP_Lab12/src/main/java/Core/History.java
@@ -1,4 +1,29 @@
 package Core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class History {
+
+    private List<EditorMemento> mementoList= new ArrayList<>();
+    private TextEditor textEditor;
+
+    public History(TextEditor textEditor){
+        this.textEditor= textEditor;
+    }
+
+    public void backup(){
+        mementoList.add(textEditor.createMemento());
+    }
+
+    public void undo(){
+        if(mementoList.isEmpty()){
+            System.out.println("Nothing to undo");
+        }
+        else{
+           EditorMemento memento= mementoList.remove(mementoList.size()-1);
+           textEditor.restoreFromMemento(memento);
+            System.out.println("State undone to previous state");
+        }
+    }
 }
